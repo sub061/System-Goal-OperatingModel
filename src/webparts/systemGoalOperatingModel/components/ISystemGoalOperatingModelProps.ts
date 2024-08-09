ad0dc491725e9bccdbace33ad4cd0693eebabc54
sub061 <subhash.tripathi@webcontentor.com>
2024-08-09 03:20:24 -0400

    first commit

---
+++ src/webparts/systemGoalOperatingModel/components/ISystemGoalOperatingModelProps.ts
@@ -0,0 +1,7 @@
+export interface ISystemGoalOperatingModelProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/systemGoalOperatingModel/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISystemGoalOperatingModelWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SystemGoalOperatingModelWebPartStrings' {
+  const strings: ISystemGoalOperatingModelWebPartStrings;
+  export = strings;
+}
